72fbd2cc671a767415aa91498fd9999d58b182b5
Prabesh Gupta <prabeshtech232@gmail.com>
2024-05-27 07:06:27 -0700

    Advance JAVA started

---
+++ School/Advance/Week 1/src/LearnNew.java
@@ -0,0 +1,13 @@
+public class LearnNew {
+    public static void main(String[] anyNames) {
+        // Instead of args we can use any name and pass value using console
+        for (String anyName : anyNames) {
+            System.out.print('\n' + anyName);
+        }
+
+        /*
+         * On console, type
+         * java LearnNew "hello" "buddy" "first" "turn" "000"
+         */
+    }
+}
---
+++ School/Advance/Week 1/src/PrintDate.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class PrintDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
